440a15c194b8ae542f0033b5827d013b3b962d13
Trần Ngọc Hùng <tranngochung.dev@gmail.com>
2026-06-01 09:57:13 +0700

    add language for send email

--- src/services/reservationService.js
+++ src/services/reservationService.js
@@ -1,7 +1,17 @@
 import api from "./api";
 
+const getCurrentLanguage = () => {
+  const locale = localStorage.getItem("app_language");
+
+  return locale;
+};
+
 const reservationService = {
-  createReservation: (body) => api.post("/reservation", body),
+  createReservation: (body) =>
+    api.post("/reservation", {
+      ...body,
+      language: getCurrentLanguage(),
+    }),
   getAll: () => api.get("/reservation"),
   acceptReservation: (id) => api.patch(`/reservation/${id}/accept`),
   rejectReservation: (id) => api.patch(`/reservation/${id}/reject`),
